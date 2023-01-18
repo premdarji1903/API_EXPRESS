8408898a2597b2dde81b76546940a68f404fb2b2
Prem darji <78422472+premdarji1903@users.noreply.github.com>
2023-01-18 21:25:46 +0530

    Update rest.js

--- rest.js
+++ rest.js
@@ -3,7 +3,7 @@ const express=require('express');
 const connectdb=require('/REST_API/db/connect');
 const mongoose=require('mongoose')
 const app= express();
-const PORT=process.env.port||2000;
+const PORT=process.env.port;
 const products=require("../REST_API/routes/product")
 app.get("/",(req,res)=>{
     res.send("server is started")
